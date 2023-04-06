1057f9826c7bc2608f56127b958e0410d8cc8c4a
bmortolano <bmortolano@gmail.com>
2023-04-06 15:10:04 -0600

    Working on Euclidean to C-Space map conversion.

---
+++ convert_Euclidean_to_Config.m
@@ -0,0 +1,38 @@
+function c_pt = convert_Euclidean_to_Config(e_pt, geom)
+    % Perform Inverse Kinematics to bring a point from Euclidean
+    % space into C-space for a Two-Link Planar Manipulator
+    % Inputs: 
+    % e_pt Euclidean-Space point in form [x, y]
+    % geom Link lengths in form [a1, a2]
+    % 
+    % Outputs: 
+    % c_pt C-Space point in form [theta1, theta2]
+
+    x = e_pt(1);
+    y = e_pt(2);
+
+    a1 = geom(1);
+    a2 = geom(2);
+
+    t2 = acosd( (x^2 + y^2 - a1^2 - a2^2) / (2 * a1 * a2) );
+    t1 = atan2d(y, x) + atan2d( a2*sin(t2), a1 + a2*cos(t2) );
+
+    c_pt = [t1 t2];
+end
+
+% function testFxn(a)
+%     disp(a)
+% end
+% 
+% function e_pt = convert_Config_to_Euclidean(c_pt, geom)
+%     % Perform Forward Kinematics
+%     % Inputs: 
+%     % c_pt C-Space point in form [theta1, theta2]
+%     % geom Link lengths in form [a1, a2]
+%     % 
+%     % Outputs: 
+%     % e_pt Euclidean-Space point in form [x, y]
+% 
+%     % TODO
+%     e_pt = 0
+% end
--- main.m
+++ main.m
@@ -1,9 +1,13 @@
 clc; clear; close all;
 
 %%
-% Create a map and potential field
+% Create robot parameters
+link_lengths = [80, 80]; % Robot link lengths
+
+%%
+% Create a euc_map for Euclidean Space
 sz = [200, 200]
-map = euclidean_map(sz);
+euc_map = obstacle_map(sz);
 
 % Create obstacles
 obst1_edges = [
@@ -16,18 +20,35 @@ obst1_edges = [
     [[30, 90], [30, 70]],
     [[30, 70], [50, 50]]];
 
-map.add_obstacle(obstacle(obst1_edges))
-map.plotMap()
-
+euc_map.add_obstacle(obstacle(obst1_edges))
+euc_map.plotMap()
 
 %%
-% figure
-% hold on
+% Create corresponding C-Space euc_map
+c_map = obstacle_map(sz);
 
-% for i = 1:size(obst1_edges, 2)
-%     edge = obst1_edges(i,:)
-%     plot([edge(1), edge(3)], [edge(2), edge(4)], 'k-')
-% end
+for e_obstacle = euc_map.obstacles
+    e_edges = e_obstacle.edges;
+    c_edges = zeros(size(e_edges));
+    
+    for i = 1:size(e_edges,1)
+        euc_edge = e_edges(i,:);
+        euc_pt1 = [euc_edge(1) euc_edge(2)];
+        euc_pt2 = [euc_edge(3) euc_edge(4)];
+        
+%         testFxn(1)
+        
+        c_pt1 = convert_Euclidean_to_Config(euc_pt1, link_lengths);
+        c_pt2 = convert_Euclidean_to_Config(euc_pt2, link_lengths);
+        
+        c_edges(i,:) = [c_pt1, c_pt2];
+    end
+    
+    c_obstacle = obstacle(c_edges);
+    c_map.add_obstacle(c_obstacle);
+end
+
+c_map.plotMap()
 
 %%
 env_field = env_pot_field()
